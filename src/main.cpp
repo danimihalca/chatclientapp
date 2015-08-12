dc307be011f6c1d907ec8d8bbe407fe3129b9c3b
Daniel Mihalca <danimihalca@gmail.com>
2015-08-12 14:24:32 +0300

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <memory>
 #include <thread>
-
+#include <vector>
 #include <ChatClient/ChatClient.hpp>
 #include <ChatClient/IChatClientListener.hpp>
 
 const char* ADDRESS = "192.168.0.3";
+std::vector<Contact> myContacts;
 
 class PrintListener : public IChatClientListener
 {
 public:
-    void onMessageReceived(const std::string& message)
+	void onMessageReceived(const Message& message)
     {
-        std::cout << "MESSAGE RECEIVED: " << message << std::endl;
+        std::cout << "MESSAGE RECEIVED FROM "<<message.getSenderId()<<":"<< message.getMessageText() << std::endl;
     }
 
     void onConnected()
@@ -29,9 +30,9 @@ public:
     {
         std::cout << "DISCONNECTED" << std::endl;
     }
-    void onLoginSuccessful()
+	void onLoginSuccessful(const UserDetails& userDetails)
     {
-        std::cout << "LOGGED IN" << std::endl;
+		std::cout << "LOGGED IN as " << userDetails.getFirstName()<< std::endl;
     }
 
     void onLoginFailed(const std::string& message)
@@ -39,25 +40,66 @@ public:
         std::cout << "LOGIN FAILED:" << message << std::endl;
 
     }
+	void onContactsReceived(const std::vector<Contact>& contacts){
+		for (auto c : contacts)
+		{
+			std::cout <<"RECEIVED:"<< c.getId() << " " << c.getUserName() << std::endl;
+			myContacts.push_back(c);
+		}
+	}
+	void onContactStateChanged(int contactId, CONTACT_STATE state)
+	{
 
+	}
+
+
+	void onRemovedByContact(int contactId)
+	{
+		std::cout << "REMOVED BY " << contactId << std::endl;
+
+	}
+	void onAddContactResponse(const std::string& userName, bool accepted)
+	{
+		std::cout << userName << " has accepted:" << (accepted ? "true" : "false") << std::endl;
+	}
+	bool onAddingByContact(const std::string& requester)
+	{
+		std::cout << "REQUEST BY " << requester << "Accept? [y/n]" << std::endl;
+		std::string m;
+		std::cin >> m;
+		if (m == "y")
+		{
+			std::cout << "ACCEPTING" << std::endl;
+			return true;
+		}
+		std::cout << "DECLINING " << std::endl;
+		return false;
+
+	}
 };
 
 
 int main()
 {
     std::unique_ptr<IChatClient> chatClient(new ChatClient());
-    {
-        std::shared_ptr<IChatClientListener> listener(new PrintListener());
-        chatClient->addChatClientListener(listener);
-    }
-    chatClient->setServerProperties(ADDRESS, 9003);
+        std::unique_ptr<IChatClientListener> listener(new PrintListener());
+        chatClient->addListener(listener.get());
+   
+    chatClient->connect(ADDRESS, 9003);
     std::string message;
     while (true)
     {
         std::cin >> message;
         if (message == "l")
         {
-            chatClient->login("user2", "pwd2");
+			std::string username;
+			std::string password;
+			std::cout << "U:";
+			std::cin >> username;
+			std::cout << "P:";
+			std::cin >> password; 
+			UserCredentials cred(username,password);
+			chatClient->login(cred);
             continue;
         }
         if (message == "d")
@@ -69,7 +111,37 @@ int main()
         {
             break;
         }
-        chatClient->sendMessage(message);
+		if (message == "m")
+		{
+			int contactId;
+			std::cout << "ContactID:";
+			std::cin >> contactId;
+			std::string message;
+			std::cout << "M:";
+			std::cin >> message;
+			chatClient->sendMessage(contactId,message);
+		}
+		if (message == "lc")
+		{
+			for (auto c : myContacts)
+			{
+				std::cout << c.getId() << " " << c.getUserName() << std::endl;
+			}
+		}
+		if (message == "r")
+		{
+			int contactId;
+			std::cout << "ContactID:";
+			std::cin >> contactId;
+			chatClient->removeContact(contactId);
+		}
+		if (message == "a")
+		{
+			std::string username;
+			std::cout << "U:";
+			std::cin >> username;
+			chatClient->addContact(username);
+		}
     }
 
     return 0;
